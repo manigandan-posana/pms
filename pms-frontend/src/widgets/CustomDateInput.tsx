5446ac86ef83488d6c6b0e96d8cd5751a6c67fd9
manigandan-posana <manigandanposana@gmail.com>
2025-12-30 12:57:52 +0530

    30-12-3

--- pms-frontend/src/widgets/CustomDateInput.tsx
+++ pms-frontend/src/widgets/CustomDateInput.tsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { DatePicker } from "@mui/x-date-pickers/DatePicker";
-import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import dayjs, { Dayjs } from "dayjs";
 import customParseFormat from "dayjs/plugin/customParseFormat";
